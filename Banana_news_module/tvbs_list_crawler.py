acc532e3c325358e7cd5b183f95a03c4fe6ac530
Hua-cin <lazyso1989@gmain.com>
2020-07-16 13:28:34 +0800

    m-repair load_file_to_list function

--- Banana_news_module/tvbs_list_crawler.py
+++ Banana_news_module/tvbs_list_crawler.py
@@ -238,6 +238,26 @@ def delay(x=1):
          # print("\rdelay {:>2d} 秒".format(t - y), end="")
          time.sleep(1)
 
+
+def load_file_to_list(path):
+   '''
+   load file for list item
+   :param path: file path
+   :return: data list
+   '''
+
+   with open(path, 'r', encoding='utf-8') as f:
+      temp = f.read()
+
+   text = temp.split('\n')
+   title_exclude_word = []
+
+   for i in text:
+      title_exclude_word.append(i)
+
+   return title_exclude_word
+
+
 if __name__ == "__main__":
    '''
    main function
